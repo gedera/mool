ee2b49b081400d366acbfb562f43b3ba2eda2a6e
Enrique Serdio <enriserdio@gmail.com>
2015-12-01 11:04:27 -0300

    cast load_average values

--- lib/mool/load_average.rb
+++ lib/mool/load_average.rb
@@ -3,11 +3,11 @@ class MoolLoadAverage
 
   def initialize
     result = File.read("/proc/loadavg").chomp.split(" ")
-    @current_loadavg       = result[0]
-    @last_5min_loadavg     = result[1]
-    @last_15min_loadavg    = result[2]
-    @thread_entities_exec  = result[3].split("/").first # Currently executing kernel scheduling entities
-    @total_thread_entities = result[3].split("/").last # Number of kernel scheduling entities that currently exist on the system
-    @last_pid_process_created = result[4]
+    @current_loadavg          = result[0].to_f
+    @last_5min_loadavg        = result[1].to_f
+    @last_15min_loadavg       = result[2].to_f
+    @thread_entities_exec     = result[3].split("/").first.to_i # Currently executing kernel scheduling entities
+    @total_thread_entities    = result[3].split("/").last.to_i # Number of kernel scheduling entities that currently exist on the system
+    @last_pid_process_created = result[4].to_i
   end
 end
