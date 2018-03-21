448db8182e59a4e5a9a430c0931f902ccfbe3d5d
g.edera <gab.edera@gmail.com>
2018-03-21 13:35:35 -0300

    Commands methods

--- lib/mool/cpu.rb
+++ lib/mool/cpu.rb
@@ -1,33 +1,35 @@
 class MoolCpu
-  PATH_PROC_CPUINFO = "/proc/cpuinfo"
+  PATH_PROC_CPUINFO = '/proc/cpuinfo'
   PROCESSORS = File.read(PATH_PROC_CPUINFO).scan(/processor\t*: (\d+)/).flatten + ["all"]
 
   attr_reader :cpu_name, :model_name, :cores, :usr, :nice, :sys, :iowait, :irq, :soft, :steal, :guest, :gnice, :idle, :total
 
   # ["all", "1", "2"]
   def initialize(process_number, opt={})
-    raise "Cpu name incorrect!. Posible values: #{MoolCpu::PROCESSORS.join(",")}" unless MoolCpu::PROCESSORS.include?(process_number.to_s)
-    result = opt.empty? ? MoolCpu.cpu_info[process_number.to_s] : opt
+    unless MoolCpu::PROCESSORS.include?(process_number.to_s)
+      raise "Cpu name incorrect!. Posible values: #{MoolCpu::PROCESSORS.join(",")}"
+    end
+    result = opt.empty? ? MoolCpu.cpuinfo[process_number.to_s] : opt
     @cpu_name = "cpu_#{process_number.to_s}"
-    @model_name = result["model_name"]
-    @cores      = result["cpu_cores"].to_i
-    @usr        = result["%usr"].to_f
-    @nice       = result["%nice"].to_f
-    @sys        = result["%sys"].to_f # This is kernel %
-    @iowait     = result["%iowait"].to_f
-    @irq        = result["%irq"].to_f
-    @soft       = result["%soft"].to_f
-    @steal      = result["%steal"].to_f
-    @guest      = result["%guest"].to_f
-    @gnice      = result["%gnice"].to_f
-    @idle       = result["%idle"].to_f
+    @model_name = result['model_name']
+    @cores      = result['cpu_cores'].to_i
+    @usr        = result['%usr'].to_f
+    @nice       = result['%nice'].to_f
+    @sys        = result['%sys'].to_f # This is kernel %
+    @iowait     = result['%iowait'].to_f
+    @irq        = result['%irq'].to_f
+    @soft       = result['%soft'].to_f
+    @steal      = result['%steal'].to_f
+    @guest      = result['%guest'].to_f
+    @gnice      = result['%gnice'].to_f
+    @idle       = result['%idle'].to_f
     @total      = @usr + @nice + @sys + @iowait + @irq + @soft + @steal + @guest
   end
 
-  def self.cpu_info
+  def self.cpuinfo
     cpu_info = {}
 
-    mpstat = File.read("|mpstat -P ALL 1 1").split("\n\n")[2].split("\n").map{|i| i.gsub(/^\S+:/, '').strip.split(/\s+/) }
+    mpstat = mpstat_command.split("\n\n")[2].split("\n").map{|i| i.gsub(/^\S+:/, '').strip.split(/\s+/) }
     mpstat_vars = mpstat.shift
     mpstat_vars.shift
     mpstat.each do |data|
@@ -37,7 +39,7 @@ class MoolCpu
       cpu_info.merge!(core_name => res)
     end
 
-    File.read(PATH_PROC_CPUINFO).gsub(/([^\n])\n([^\n])/, '\1 \2').scan(/processor\t*: (\d+).*model name\t*: (.*) stepping.*cpu cores\t*: (\d+)/).each do |v|
+    cpuinfo_command.gsub(/([^\n])\n([^\n])/, '\1 \2').scan(/processor\t*: (\d+).*model name\t*: (.*) stepping.*cpu cores\t*: (\d+)/).each do |v|
       cpu_info[v[0]]["model_name"] = v[1]
       cpu_info[v[0]]["cpu_cores"] = v[2]
     end
@@ -46,11 +48,30 @@ class MoolCpu
   end
 
   def self.processors
-    File.read(PATH_PROC_CPUINFO).scan(/processor\t*: (\d+)/).flatten + ["all"]
+    cpuinfo_command.scan(/processor\t*: (\d+)/).flatten + ["all"]
   end
-
 
   def self.all
-    MoolCpu.cpu_info.map{ |key, value| MoolCpu.new(key, value) }
+    MoolCpu.cpuinfo.map{ |key, value| MoolCpu.new(key, value) }
   end
+
+  def self.cpuinfo_command
+    File.read(MoolCpu::PATH_PROC_CPUINFO)
+  end
+
+  def self.mpstat_command
+    File.read('|mpstat -P ALL 1 1')
+  end
+
+  # Maybe it's useful in the future
+  # def self.cpuinfo_hash
+  #   hash = {}
+
+  #   cpuinfo_command.split("\n").each do |a|
+  #     next if a.empty?
+  #     result = a.delete("\t").delete('\"').split(':')
+  #     hash[result[0].tr(' ', '_')] = (result[1].strip if result[1].present?)
+  #   end
+  #   hash
+  # end
 end
--- lib/mool/disk.rb
+++ lib/mool/disk.rb
@@ -4,10 +4,11 @@ class MoolDisk
   attr_accessor :path, :major, :minor, :devname, :devtype, :size, :swap, :mount_point, :file_system, :total_block, :block_used, :block_free, :partitions, :slaves, :unity
 
   def initialize(dev_name)
-    @path = PATH_DEV_BLOCK.select{ |entry| ( File.read("#{entry}/dev").include?(dev_name)) ||
-                                           ( File.read("#{entry}/uevent").include?(dev_name)) ||
-                                           ( File.exist?("#{entry}/dm/name") &&
-                                             File.read("#{entry}/dm/name").include?(dev_name)) }.first
+    @path = PATH_DEV_BLOCK.select { |entry|
+      (File.read("#{entry}/dev").include?(dev_name)) ||
+        (File.read("#{entry}/uevent").include?(dev_name)) ||
+        (File.exist?("#{entry}/dm/name") &&
+         File.read("#{entry}/dm/name").include?(dev_name)) }.first
     raise "Does't exist #{dev_name}!" if @path.nil?
     read_uevent
     logical_name
@@ -19,33 +20,42 @@ class MoolDisk
   end
 
   def mounting_point
-    @mount_point ||= File.read("/proc/mounts").scan(/#{@logical_name} (\S+)/).flatten.first if File.read("/proc/mounts").include?(@logical_name)
+    @mount_point ||= MoolDisk.mount_command.scan(/#{@logical_name} (\S+)/).flatten.first if MoolDisk.mount_command.include?(@logical_name)
   end
 
   def file_system
-    @file_system ||= (Dir.glob("/sys/fs/**/*").select{|a| a.include?(@devname)}.first.split("/")[3] rescue nil)
+    @file_system ||= (MoolDisk.file_system_command.select { |a| a.include?(@devname) }.first.split("/")[3] rescue nil)
   end
 
   def logical_name
-    @logical_name ||= File.exists?("#{@path}/dm/name")? File.read("#{@path}/dm/name").chomp : @devname
+    lname = MoolDisk.logical_name_command(@path)
+    lname.present? ? lname : @devname
   end
 
   def read_uevent
-    @major, @minor, @devname, @devtype = File.read("#{@path}/uevent").scan(/.*=(\d+)\n.*=(\d+)\n.*=(\S+)\n.*=(\w+)\n/).flatten
+    @major, @minor, @devname, @devtype = MoolDisk.uevent_command(@path).scan(/.*=(\d+)\n.*=(\d+)\n.*=(\S+)\n.*=(\w+)\n/).flatten
   end
 
-  def dev; @major + @minor; end
+  def dev
+    @major + @minor
+  end
 
-  def is_disk?; @devtype == "disk"; end
+  def is_disk?
+    @devtype == 'disk'
+  end
 
-  def is_partition?; @devtype == "partition"; end
+  def is_partition?
+    @devtype == 'partition'
+  end
 
-  def swap; @swap ||= File.read("/proc/swaps")[/#{@logical_name} /].present?; end
+  def swap
+    @swap ||= MoolDisk.swap_command(@logical_name).present?
+  end
 
   def capacity
-    unless (defined?(@total_block) && defined?(@block_used) && defined?(@block_free))
-      result = `df`.scan(/(#{@logical_name}|#{@devname})\s+(\d+)\s+(\d+)\s+(\d+)\s+(\S+)/).flatten
-      @total_block = File.read("#{@path}/size").chomp.to_f * Mool::BLOCK_SIZE
+    unless defined?(@total_block) && defined?(@block_used) && defined?(@block_free)
+      result = MoolDisk.df_command.scan(/(#{@logical_name}|#{@devname})\s+(\d+)\s+(\d+)\s+(\d+)\s+(\S+)/).flatten
+      @total_block = MoolDisk.capacity_partition_command(@path).chomp.to_f * Mool::BLOCK_SIZE
       @block_used  = result[2].to_f * Mool::BLOCK_SIZE
       @block_free  = @total_block - @block_used
     end
@@ -55,8 +65,8 @@ class MoolDisk
     unless defined? @partitions
       @partitions = []
       if is_disk?
-        Dir.glob("#{@path}/#{@devname}*").each do |part|
-          @partitions << MoolDisk.new(part.split("/").last)
+        MoolDisk.partitions_command(@path, @devname).each do |part|
+          @partitions << MoolDisk.new(part.split('/').last)
         end
       end
     end
@@ -81,29 +91,107 @@ class MoolDisk
     @block_free / @total_block
   end
 
+  def to_b
+    Mool.parse_to(self,
+                  ['@total_block',
+                   '@block_used',
+                   '@block_free'],
+                  Mool::BYTES)
+  end
+
+  def to_kb
+    Mool.parse_to(self,
+                  ['@total_block',
+                   '@block_used',
+                   '@block_free'],
+                  Mool::KBYTES)
+  end
+
+  def to_mb
+    Mool.parse_to(self,
+                  ['@total_block',
+                   '@block_used',
+                   '@block_free'],
+                  Mool::MBYTES)
+  end
+
+  def to_gb
+    Mool.parse_to(self,
+                  ['@total_block',
+                   '@block_used',
+                   '@block_free'],
+                  Mool::GBYTES)
+  end
+
+  def self.df_command
+    `df`
+  end
+
+  def self.mount_command
+    File.read('/proc/mounts')
+  end
+
+  def self.file_system_command
+    Dir.glob('/sys/fs/**/*')
+  end
+
+  def self.logical_name_command(path)
+    File.exist?("#{path}/dm/name") ? File.read("#{path}/dm/name").chomp : nil
+  end
+
+  def self.uevent_command(path)
+    File.read("#{path}/uevent")
+  end
+
+  def self.swap_command(lname = nil)
+    result = File.read('/proc/swaps')
+    lname.present? ? result[/#{lname} /] : result
+  end
+
+  def self.capacity_partition_command(path)
+    File.read("#{path}/size")
+  end
+
+  def self.partitions_command(path, devname)
+    Dir.glob("#{path}/#{devname}*")
+  end
+
+  def self.dev_block_command
+    Dir.glob('/sys/dev/block/*')
+  end
+
+  def self.real_path_block_command(entry)
+    `readlink -f #{entry}`.chomp
+  end
+
+  def self.real_path_command_exist?(real_path)
+    File.exist?("#{real_path}/partition")
+  end
+
+  def self.slaves_command(real_path)
+    Dir.glob("#{real_path}/slaves/*")
+  end
+
   def self.all
     disks = []
 
-    PATH_DEV_BLOCK.each do |entry|
-      real_path = `readlink -f #{entry}`.chomp
-      disks << MoolDisk.new(entry.split("/").last) if (not real_path.include?("virtual")) &&
-                                                      (not real_path.include?("/sr")) &&
-                                                      (not File.exist?("#{real_path}/partition")) &&
-                                                      Dir.glob("#{real_path}/slaves/*").empty?
+    MoolDisk.dev_block_command.each do |entry|
+      real_path = MoolDisk.real_path_block_command(entry)
+      if !real_path.include?('virtual') &&
+         !real_path.include?('/sr') &&
+         !MoolDisk.real_path_command_exist?(real_path) &&
+         MoolDisk.slaves_command(real_path).empty?
+        disks << MoolDisk.new(entry.split('/').last)
+      end
     end
 
-    disks.each{ |disk| disk.partitions.each { |part| part.partitions and part.slaves }}
-    disks.each{ |disk| disk.slaves.each { |part| part.partitions and part.slaves }}
+    disks.each { |disk| disk.partitions.each { |part| part.partitions && part.slaves }}
+    disks.each { |disk| disk.slaves.each { |part| part.partitions && part.slaves }}
     disks
   end
 
-  def to_b;  Mool.parse_to(self, ["@total_block", "@block_used", "@block_free"], Mool::BYTES);  end
-  def to_kb; Mool.parse_to(self, ["@total_block", "@block_used", "@block_free"], Mool::KBYTES); end
-  def to_mb; Mool.parse_to(self, ["@total_block", "@block_used", "@block_free"], Mool::MBYTES); end
-  def to_gb; Mool.parse_to(self, ["@total_block", "@block_used", "@block_free"], Mool::GBYTES); end
-
   def self.swap
-    result = File.read("/proc/swaps").scan(/.*\n\/dev\/(\S+)/).flatten.first
+    result = MoolDisk.swap_command.scan(/.*\n\/dev\/(\S+)/).flatten.first
     MoolDisk.new(result) unless result.nil?
   end
 
@@ -112,6 +200,6 @@ class MoolDisk
     result.each do |disk|
       result += (disk.partitions + disk.slaves + (disk.partitions + disk.slaves).collect{|p| p.partitions + p.slaves }.flatten)
     end
-    result.reject(&:blank?).select{|d| (d.partitions + d.slaves).blank? }
+    result.reject(&:blank?).select{ |d| (d.partitions + d.slaves).blank? }
   end
 end
